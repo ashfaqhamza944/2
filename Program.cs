f9eb05695c453886180904b61f2a18d09bdd2271
ashfaqhamza944 <90825192+ashfaqhamza944@users.noreply.github.com>
2021-11-26 09:58:07 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 
-namespace _2
+namespace q_no_59
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("a");
+int n = 22 / 8;
+Console.WriteLine(n);
+
+//(b)
+Console.WriteLine("b");
+int n1 = 99 / 8 + 21 / 11 * 7;
+Console.WriteLine(n1);
+
+//(c)
+Console.WriteLine("c");
+double a = 4, b = 3, p = 8, q = 2;
+double n2 = q / a - p / b;
+Console.WriteLine(n2);
+
+//(d)
+Console.WriteLine("d");
+int a1 = 6, b1 = 3, p1 = 8, q1 = 2;
+double n3 = p1 / a1 + q1 / b1;
+Console.WriteLine(n3);
+
+//(e)
+Console.WriteLine("e");
+float a2 = 3.3f, b2 = 2.7f, p2 = 8.7f, q2 = 5.4f;
+Convert.ToInt32(a2);
+Convert.ToInt32(b2);
+Convert.ToInt32(p2);
+Convert.ToInt32(q2);
+double n4 = p2 / b2 + q2 / a2;
+Console.WriteLine(n4);
+
+//(f)
+Console.WriteLine("f");
+int n5 = 11 + 7 / 4 + 98 % 6 * 3;
+Console.WriteLine(n5);
+
+//(g)
+Console.WriteLine("g");
+int n6 = 11 / 31 % 8 * 5 - 12;
+Console.WriteLine(n6);
+
+//(h)
+Console.WriteLine("h");
+int p3 =8;
+int q3 = 5;
+int n7 = q3 * p3 + 3 * p3 * p3 - (q3 % 3) * p3 * p3 * p3;
+Console.WriteLine(n7);
+
+//(i)
+Console.WriteLine("i");
+int p4 = 5;
+int q4 = 9;
+int n8 = (q4 % 2)p4 + 6*p4(q4%3) +(q4 % 4) * p4 * p4 * p4;
+Console.WriteLine(n8);
+
+//(j)
+Console.WriteLine("j");
+int n9 = 3, a5= 6, b5 = 2;
+int n10=n9 * (a5 + b5);
+a5--;
+n10 = n10 - a5;
+Console.WriteLine(n10)
         }
     }
 }
